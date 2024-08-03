6927cbe2dc13533aff275d725804ad8f0c9fd616
sadiquesq <sadiquesanu1@gmail.com>
2024-08-03 12:53:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function sendMail(){
+    
+    var params={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    };
+    const serviceID = "service_8xu3jvj";
+    const templateID = "template_apvabla";
+
+    emailjs
+    .send(serviceID,templateID,params)
+    .then((res)=>{
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("message").value="";
+        console.log(res);
+        alert("Your Message Sent Successfully");
+        
+    })
+    .catch((err)=>console.log(err));
+}
